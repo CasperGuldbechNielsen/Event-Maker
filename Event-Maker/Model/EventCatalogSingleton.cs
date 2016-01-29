10b885d0ab27645af4472042040b1eff3915b000
CasperGuldbechNielsen <casp674d@edu.easj.dk>
2016-01-29 08:19:54 +0100

    Added code

--- Event-Maker/Model/EventCatalogSingleton.cs
+++ Event-Maker/Model/EventCatalogSingleton.cs
@@ -1,12 +1,19 @@
-﻿namespace Event_Maker.Model
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+
+namespace Event_Maker.Model
 {
     public class EventCatalogSingleton
     {
         private static EventCatalogSingleton _instance = null;
 
+        public ObservableCollection<Event> Events { get; set; }
+
         private EventCatalogSingleton()
         {
-            Event myEvent = new Event();
+            Events = new ObservableCollection<Event>();
+            LoadEventAsync();
         }
 
         public static EventCatalogSingleton GetInstance()
@@ -18,6 +25,26 @@
                 _instance = new EventCatalogSingleton();
                 return _instance;
             }
+        }
+
+        public void LoadEventAsync()
+        {
+            Events.Add(new Event(1, "Shrink", "Need to see the shrink", "Køge", new DateTime(2016, 01, 29)));
+        }
+
+        public void AddEvent()
+        {
+            
+        }
+
+        public void DeleteEvent()
+        {
+            
+        }
+
+        public void EditEvent()
+        {
+            
         }
     }
 }
--- Event-Maker/ViewModel/EventViewModel.cs
+++ Event-Maker/ViewModel/EventViewModel.cs
@@ -1,7 +1,82 @@
-﻿namespace Event_Maker.ViewModel
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Event_Maker.Model;
+
+namespace Event_Maker.ViewModel
 {
-    public class EventViewModel
+    public class EventViewModel : INotifyPropertyChanged
     {
-         
+
+        private int _id;
+        private string _name;
+        private string _description;
+        private string _place;
+        private DateTimeOffset _date;
+        private TimeSpan _time;
+
+        public EventCatalogSingleton MyCatalogSingleton { get; set; }
+
+        public EventViewModel()
+        {
+            MyCatalogSingleton = EventCatalogSingleton.GetInstance();
+            DateTime dateTime = System.DateTime.Now;
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Place
+        {
+            get { return _place; }
+            set { _place = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTimeOffset Date
+        {
+            get { return _date; }
+            set { _date = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TimeSpan Time
+        {
+            get { return _time; }
+            set { _time = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] String propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
